3daa9ac0ceaba7dc53cb154cd9c1ef501448cf30
Henrique Gamborgi <henriquegamborgi@gmail.com>
2024-11-05 02:20:08 -0500

    Insert Model Viewer
    In this commit, I added the model and the model viewer tag to make it work dynamically.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log("JavaScript file is linked."); // this will ensure my .js file is connected
+(() => {
+  const hotspots = document.querySelectorAll(".Hotspot");
+  
+  function showInfo(e) {
+    console.log(e.currentTarget.slot);
+    let selected = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`);
+    //button[slot="hotspot-1"] > div
+    gsap.to(selected, 1, {autoAlpha:1});			
+  }
+  
+  function hideInfo(e) {
+    //console.log(e.currentTarget.slot);
+    let selected = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`);
+    gsap.to(selected, 1, {autoAlpha:0});
+  }
+  
+  hotspots.forEach(hotspot => {
+    // The hover event is triggered with click/touch events on mobile phone, because you can not simply hover an element on a touch screen.
+    hotspot.addEventListener("mouseover", showInfo);
+    hotspot.addEventListener("mouseout", hideInfo);
+  });
+
+  model.addEventListener("load", loaded);
+})()
+
+
+
